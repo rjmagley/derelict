4edace8565d9d8d39d389d5819574a55aee8603f
Ryan Magley <ryan.magley@gmail.com>
2023-12-29 19:22:06 -0600

    Comments for later re: modifier stacking

--- powers/alacrity.py
+++ powers/alacrity.py
@@ -27,9 +27,17 @@ class AlacrityPower(BasePower):
         self.handler_type = None
         self.is_targeted = False
 
-    # currently this can stack - maybe it shouldn't
+    # currently this can stack - maybe it shouldn't?
+    # making it not stack would require keeping track of the source of 
+    # modifiers, because there are situations where one property can be
+    # changed by multiple sources for better or worse - player buffs their speed,
+    # uses another power that also causes an incidental speed buff, and is
+    # affected by an enemy's speed debuff
     def cast(self):
         self.caster.psy_points -= self.power_cost
+        # the 20 below is a magic number - when the player has psy-related 
+        # skills, it might be cool to do 20 + some number based on that
+        # skill modifier
         self.caster.modifiers.append(
             Modifier(ModifierProperty.MOVEMENT_SPEED, -5, 20, False, " Ala ", color.bright_yellow, color.blue)
         )
