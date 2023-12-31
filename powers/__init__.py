a7dcd37ba8b5fda20a72f4a5b874d8c47852243b
Ryan Magley <ryan.magley@gmail.com>
2023-12-31 15:58:19 -0600

    Adding power types

---
+++ powers/__init__.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+
+from enum import StrEnum
+
+class PowerType(StrEnum):
+    OFFENSE = "offensive"
+    DEFENSE = "defensive"
+    MANIPULATION = "manipulative"
+    ENHANCE = "enhancement"
