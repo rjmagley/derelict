2171722847a0a5f29d8e6f99344d3275316c0b8d
Ryan Magley <ryan.magley@gmail.com>
2024-04-28 18:02:31 -0500

    Change render order; enemies always render above ammo/items

--- render_order.py
+++ render_order.py
@@ -2,8 +2,9 @@ from enum import auto, Enum
 
 
 class RenderOrder(Enum):
-    EFFECT = auto()
     CORPSE = auto()
-    ITEM = auto()
     PICKUP = auto()
+    ITEM = auto()
+    EFFECT = auto()
     COMBATANT = auto()
+    
